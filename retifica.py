4c6ea8ebb0de1ed33da4fd0bcca779ed4b24ba30
AngeloCamboim <angelocmboim@gmail.com>
2019-05-14 00:28:58 -0300

    Segunda versão da aplicação.
    Retifica de bloco adicionada, e anotações de mais coisas a serem implementadas.

--- retifica.py
+++ retifica.py
@@ -1,4 +1,6 @@
+import sys
 print("--- Bem-vindo ao programa de Retífica ---")
+print("--- Primeiramente, informe as medidas do munhão e do moente")
 print("Informe as medidas STD, e as medidas encontradas no munhão e moente")
 
 primeira_medida = 0.250
@@ -31,6 +33,30 @@ elif (resultado2 > primeira_medida and resultado < segunda_medida):
 elif (resultado2 > segunda_medida and resultado2 < terceira_medida):
     print("A folga é de {:.3f}, retificar para {:.3f}".format(resultado2,terceira_medida))
 else:
-    print("A folga é de {:.3f}, retificar para {:.3f}".format(resultado2,quarta_medida))
+    print("A folga é de {:.3f}, retificar para {:.3f} \n".format(resultado2,quarta_medida))
+
+print("--- Agora, serão avaliados os cilindros. Informe a pior medida encontrada ---")
+cilindro = float(input("Medida do cilindro STD: "))
+
+cilindroMedido = float(input("Informe a medida do cilindro mais gasto: "))
+
+resultado3 = cilindroMedido - cilindro
+
+if (resultado3 <= primeira_medida):
+    print("O desgaste foi de {:.3f}, retificar para {:.3f}".format(resultado3,primeira_medida))
+elif (resultado3 > primeira_medida and resultado3 < segunda_medida):
+    print("O desgaste foi de {:.3f}, retificar para {:.3f}".format(resultado3,segunda_medida))
+elif (resultado3 > segunda_medida and resultado3 < terceira_medida):
+    print("O desgaste foi de {:.3f}, retificar para {:.3f}".format(resultado3,terceira_medida))
+else:
+    print("O desgaste foi de {:.3f}, retificar para {:.3f} \n".format(resultado3,quarta_medida))
+
+
 
 print("--- OBRIGADO POR USAR VALHALLA SOFTWARE ---")
+
+
+
+#sys.exit()
+#Adicionar a fala para substituir vira ou bloco
+#Adicionar um relatório completo no final
