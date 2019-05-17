f5efccef0487d4d7276a0bbb27e595a4f2e0d6c9
AngeloCamboim <angelocmboim@gmail.com>
2019-05-16 23:10:56 -0300

    Quarta versão da aplicação

--- retifica.py
+++ retifica.py
@@ -1,5 +1,4 @@
-import sys
-print("--- Bem-vindo ao programa de Retífica ---")
+print("\33[1;34m--- Bem-vindo ao programa de Retífica ---")
 print("--- Primeiramente, informe as medidas do munhão e do moente")
 print("Informe as medidas STD, e as medidas encontradas no munhão e moente")
 
@@ -13,33 +12,33 @@ munhaoMedido = float(input("Medida encontrada no munhão: "))
 
 resultado = munhao - munhaoMedido
 if (resultado <= primeira_medida):
-    print("A folga é de {:.3f}, retificar para {:.3f}".format(resultado,primeira_medida))
+    print("\33[1;32mA folga é de {:.3f}, retificar para {:.3f}".format(resultado,primeira_medida))
 elif (resultado > primeira_medida and resultado < segunda_medida):
-    print("A folga é de {:.3f}, retificar para {:.3f}".format(resultado,segunda_medida))
+    print("\33[1;32mA folga é de {:.3f}, retificar para {:.3f}".format(resultado,segunda_medida))
 elif (resultado > segunda_medida and resultado < terceira_medida):
-    print("A folga é de {:.3f}, retificar para {:.3f}".format(resultado,terceira_medida))
+    print("\33[1;32mA folga é de {:.3f}, retificar para {:.3f}".format(resultado,terceira_medida))
 elif (resultado > terceira_medida and resultado < quarta_medida):
-    print("A folga é de {:.3f}, retificar para {:.3f}".format(resultado, quarta_medida))
+    print("\33[1;32mA folga é de {:.3f}, retificar para {:.3f}".format(resultado, quarta_medida))
 else:
-    print("Substituir o virabrequim")
+    print("\33[1;31mSubstituir o virabrequim")
 
-moente = float(input("Medida do moente STD: "))
+moente = float(input("\33[1;34mMedida do moente STD: "))
 moenteMedido = float(input("Medida encontrada no moente: "))
 
 resultado2 = moente - moenteMedido
 
 if (resultado2 <= primeira_medida):
-    print("A folga é de {:.3f}, retificar para {:.3f}".format(resultado2,primeira_medida))
+    print("\33[1;32mA folga é de {:.3f}, retificar para {:.3f}".format(resultado2,primeira_medida))
 elif (resultado2 > primeira_medida and resultado < segunda_medida):
-    print("A folga é de {:.3f}, retificar para {:.3f}".format(resultado2,segunda_medida))
+    print("\33[1;32mA folga é de {:.3f}, retificar para {:.3f}".format(resultado2,segunda_medida))
 elif (resultado2 > segunda_medida and resultado2 < terceira_medida):
-    print("A folga é de {:.3f}, retificar para {:.3f}".format(resultado2,terceira_medida))
+    print("\33[1;32mA folga é de {:.3f}, retificar para {:.3f}".format(resultado2,terceira_medida))
 elif (resultado2 > terceira_medida and resultado2 < quarta_medida):
-    print("A folga é de {:.3f}, retificar para {:.3f} \n".format(resultado2, quarta_medida))
+    print("\33[1;32mA folga é de {:.3f}, retificar para {:.3f} \n".format(resultado2, quarta_medida))
 else:
-    print("Substitua o virabrequim")
+    print("\33[1;31mSubstitua o virabrequim")
 
-print("--- Agora, serão avaliados os cilindros. ---")
+print("\33[1;34m--- Agora, serão avaliados os cilindros. ---")
 cilindro = float(input("Medida do cilindro STD: "))
 
 cilindroMedido = float(input("Informe a medida do cilindro mais gasto: "))
@@ -47,15 +46,14 @@ cilindroMedido = float(input("Informe a medida do cilindro mais gasto: "))
 resultado3 = cilindroMedido - cilindro
 
 if (resultado3 <= primeira_medida):
-    print("O desgaste foi de {:.3f}, retificar para {:.3f}".format(resultado3,primeira_medida))
+    print("\33[1;32mO desgaste foi de {:.3f}, retificar para {:.3f}".format(resultado3,primeira_medida))
 elif (resultado3 > primeira_medida and resultado3 < segunda_medida):
-    print("O desgaste foi de {:.3f}, retificar para {:.3f}".format(resultado3,segunda_medida))
+    print("\33[1;32mO desgaste foi de {:.3f}, retificar para {:.3f}".format(resultado3,segunda_medida))
 elif (resultado3 > segunda_medida and resultado3 < terceira_medida):
-    print("O desgaste foi de {:.3f}, retificar para {:.3f}".format(resultado3,terceira_medida))
+    print("\33[1;32mO desgaste foi de {:.3f}, retificar para {:.3f}".format(resultado3,terceira_medida))
 elif (resultado3 > terceira_medida and resultado3 < quarta_medida):
-    print("O desgaste foi de {:.3f}, retificar para {:.3f} \n".format(resultado3,quarta_medida))
+    print("\33[1;32mO desgaste foi de {:.3f}, retificar para {:.3f} \n".format(resultado3,quarta_medida))
 else:
-    print("Substitua o bloco do motor")
+    print("\33[1;31mSubstitua o bloco do motor")
 
-print("--- OBRIGADO POR USAR VALHALLA SOFTWARE ---")
-#sys.exit()
+print("\33[1;36m--- OBRIGADO POR USAR VALHALLA SOFTWARE ---")
